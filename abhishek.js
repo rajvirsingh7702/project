55edef9147145f074c25ba93cf4673bea280b76d
abhisheksingh7702 <abhishek225669@gmail.com>
2020-10-19 15:54:11 +0530

    create commit as first directory

---
+++ abhishek.js
@@ -0,0 +1,19 @@
+app.post('8000',function(err,res){
+    if(err) {
+      console.log("program is woringh");}
+     return  console.log("program is woringh");
+  });
+  
+  app.post('8000',function(err,res){
+    if(err) {
+      console.log("program is woringh");}
+     return  console.log("program is woringh");
+  });
+  
+  app.post('8000',function(err,res){
+    if(err) {
+      console.log("program is woringh");}
+     return  console.log("program is woringh");
+  });
+  
+      
---
+++ app.js
@@ -0,0 +1,13 @@
+app.post('8000',function(err,res){
+    if(err) {
+      console.log("program is woringh");}
+     return  console.log("program is woringh");
+  });
+  
+  app.post('8000',function(err,res){
+    if(err) {
+      console.log("program is woringh");}
+     return  console.log("program is woringh");
+  });
+  
+    
